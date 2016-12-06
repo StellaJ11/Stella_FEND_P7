8d49dfb1875bbcf31c723003cc64f4cebd1614b4
Stella Jiang <stella0101kong@gmail.com>
2016-12-05 21:34:52 -0500

    Fix FourSquare Ajax issues

--- app.js
+++ app.js
@@ -49,22 +49,45 @@ var ViewModel = function() {
     };    
 
     // //Run FourSquare API calls to get data
-    self.places().forEach(function(place) {
+    // self.filteredLocations().forEach(function(place) {
+        
+    //  });
+
+    //Add event listener for responsiveness
+    // google.maps.event.addDomListener(window, 'resize', function() {
+    //     var center = map.getCenter();
+    //     google.maps.event.trigger(map, "resize");
+    //     map.setCenter(center); 
+    // });
+
+};
+       
+
+// This function populates the infowindow when the marker is clicked. 
+function populateInfoWindow(marker, infowindow) {
+    console.log(marker);
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.setContent('loading...');
+        infowindow.open(map, marker);
+
         var client_id = 'BHU3FSEQDCGVDVFR1MYUNCKJK0HIUZ4SSLPMLDNQTWJCQBNG',
             client_secret = 'QWJVQ0MLI1U4L0ZVHB4W5OJKPYGQEK2GPBF4LQNQJHVBV45X',
             query = "restaurant",
             location, 
-            venue,
+            venue;
 
         $.ajax({
             url:'https://api.foursquare.com/v2/venues/search',
             dataType: 'json',
-            data:   'limit=1' +
-                    '&ll=40.707496,-73.990774' +
-                    '&query=' + place.title +
-                    '&client_id='+ client_id +
-                    '&client_secret='+ client_secret +
-                    '&v=20161113'
+            data:   {
+                limit: '1',
+                ll: '40.707496,-73.990774',
+                query: marker.title,
+                client_id: client_id,
+                client_secret: client_secret,
+                v: '20161113'
+            }
          }).done(function (data) { 
              // If incoming data has a venues object set the first one to the var venue
              venue = data.response.hasOwnProperty("venues") ? data.response.venues[0] : '';
@@ -73,36 +96,24 @@ var ViewModel = function() {
              location = venue.hasOwnProperty('location') ? venue.location : '';
              // If new location has prop address then set the observable address to that or blank
              if (location.hasOwnProperty('address')) {
-                 place.address = location.address || '';
+                 var address = location.address || '';
              }
 
-             }).fail(function (e) {
-                 infowindow.setContent('<h5>Foursquare data is unavailable.</h5>');
-                 self.showMessage(true);
-             });
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div><p>' + address + '</p>');
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
+
+         }).fail(function (e) {
+             infowindow.setContent('<h5>Foursquare data is unavailable.</h5>');
+             self.showMessage(true);
          });
 
-    //Add event listener for responsiveness
-    google.maps.event.addDomListener(window, 'resize', function() {
-        var center = map.getCenter();
-        google.maps.event.trigger(map, "resize");
-        map.setCenter(center); 
-    });
 
-};
-       
-
-// This function populates the infowindow when the marker is clicked. 
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-        });
+        
     }
 }
 
@@ -128,6 +139,7 @@ var initMap = function() {
         // Get the position from the location array.
         var position = vm.filteredLocations()[i].location;
         var title = vm.filteredLocations()[i].title;
+        console.log(vm.filteredLocations()[i].address);
         // Create a marker per location, and put into markers array.
         var marker = new google.maps.Marker({
             position: position,
